52285b4374dc6cd3e0bebaff2017cc7acac750a6
Nickjohn Ibuyat <nickjohnibuyat@gmail.com>
2025-04-07 02:14:12 +0800

    Initial Authors Routes

--- server.js
+++ server.js
@@ -5,15 +5,18 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const indexRouter = require("./routes/index");
-const mongoose = require("mongoose");
+const authorRouter = require("./routes/authors");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not defined in your environment variables.");
@@ -26,5 +29,6 @@ db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Mongoose"));
 
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
 
 app.listen(process.env.PORT || 3000);
